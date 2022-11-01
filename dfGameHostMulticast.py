9777286ec9676a15f81bdc5dc9b5bf9c9ebc7a4c
TampaGuard <106408407+TampaGuard@users.noreply.github.com>
2022-11-01 17:09:31 -0400

    Moved structure functions to new file

--- dfGameHostMulticast.py
+++ dfGameHostMulticast.py
@@ -19,66 +19,8 @@ Process:
 import sys, math, time, socket, traceback, string
 from dfNPCEngine import DeathFinderNPC
 from dfMapRender import MapLoad
+from dfStruct import *
 from random import randint, choice
-
-def building_pog(x,y,w,l,d="left",i=True):
-  build = [(x,y)]
-  if i == True:
-    inside = square_pog(x+1,y+1,w-1,l-1)
-  else:
-    inside = []
-  door = None
-
-  for X in range(w):
-    build.append((X+x,y))
-    build.append((X+x,y+l))
-  for Y in range(l):
-    build.append((x,Y+y))
-    build.append((x+w,Y+y))
-  build.append((x+w,y+l))
-
-  if d == "left":
-    door = (x, int(y+(l/2)))
-  elif d == "right":
-    door = (x+w, int(y+(l/2)))
-  elif d == "top":
-    door = (int(x+(w/2)), y)
-  elif d == "bottom":
-    door = (int(x+(w/2)), y+l)
-  elif d == "random":
-    door = choice(build)
-  if door:
-    build.remove(door)
-    if i == True:
-      inside.append(door)
-  return build, door, inside
-
-def circle_pog(x,y,r):
-  circle = []
-  for locx in range(x-(r*2),x+(r*2)):
-    for locy in range(y-(r*2),y+(r*2)):
-      radi = math.sqrt(((x-locx)**2) + ((y-locy)**2))
-      if radi < r:
-        circle.append((locx,locy))
-  for locx in range(x-(r*2),x+(r*2)):
-    for locy in range(y-(r*2),y+(r*2)):
-      radi = math.sqrt(((x-locx)**2) + ((y-locy)**2))
-      if radi < r-1:
-        circle.remove((locx,locy))
-  return circle
-
-def magicwall_pog(y,w):
-  magicwall = []
-  for x in range(0,w+1):
-    magicwall.append((x,y))
-  return magicwall
-
-def square_pog(x,y,w,l):
-  block = []
-  for X in range(w):
-    for Y in range(l):
-      block.append((x+X,y+Y))
-  return block
 
 def _decompress(comp):
   comp = comp.replace("3","555").replace("4", "."*4).replace("5"," "*5).replace(";"," ").replace("6","#"*6).replace("!n","\n")
