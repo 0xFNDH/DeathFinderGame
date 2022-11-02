dfb7094f661d75e3941e80b6634222a36b888055
TampaGuard <106408407+TampaGuard@users.noreply.github.com>
2022-11-01 20:46:58 -0400

    Better programming

--- dfStruct.py
+++ dfStruct.py
@@ -1,13 +1,12 @@
 import math
 from random import randint, choice
 
-def building_pog(x,y,w,l,d="left",i=True):
+def building_pog(x,y,w,l,d="left",inside=True):
   build = [(x,y)]
-  if i == True:
+  if inside == True:
     inside = square_pog(x+1,y+1,w-1,l-1)
   else:
     inside = []
-  door = None
 
   for X in range(w):
     build.append((X+x,y))
@@ -16,22 +15,28 @@ def building_pog(x,y,w,l,d="left",i=True):
     build.append((x,Y+y))
     build.append((x+w,Y+y))
   build.append((x+w,y+l))
-
-  if d == "left":
-    door = (x, int(y+(l/2)))
-  elif d == "right":
-    door = (x+w, int(y+(l/2)))
-  elif d == "top":
-    door = (int(x+(w/2)), y)
-  elif d == "bottom":
-    door = (int(x+(w/2)), y+l)
-  elif d == "random":
-    door = choice(build)
-  if door:
+  
+  door = []
+  if d != None:
+    if d == "left":
+      door = (x, int(y+(l/2)))
+    elif d == "right":
+      door = (x+w, int(y+(l/2)))
+    elif d == "top":
+      door = (int(x+(w/2)), y)
+    elif d == "bottom":
+      door = (int(x+(w/2)), y+l)
+    elif d == "random":
+     door = choice(build)
     build.remove(door)
-    if i == True:
-      inside.append(door)
-  return build, door, inside
+    inside.append(door)
+  
+  newbuild = []
+  for b in build:
+    if b not in newbuild:
+      newbuild.append(b)
+  
+  return newbuild, door, inside
 
 def circle_pog(x,y,r):
   circle = []
