2479199927a8ff70bee6a10ece782468c6bca082
TampaGuard <106408407+TampaGuard@users.noreply.github.com>
2022-11-06 18:32:38 -0500

    New Map Objects
    Forest Swamp Update:
    Added logs, branches, bushes, leafs, and water

--- dfMapRender.py
+++ dfMapRender.py
@@ -1,4 +1,6 @@
-def MapLoad(mapfile="map_DONOTEDIT.txt", ymin=0, ymax=100):
+
+
+def dfMapLoad(mapfile="blight_map.txt", ymin=0, ymax=100, width=40):
   mapfile = open(mapfile,"r")
   map_data = mapfile.read().split("\n")[ymin:ymax]
   mapfile.close()
@@ -6,40 +8,41 @@ def MapLoad(mapfile="map_DONOTEDIT.txt", ymin=0, ymax=100):
   dfWall = []
   dfDark = []
   dfSpawn = []
-  width = 40 # dfGameHost Map Size
+  dfBranch = []
+  dfBush = []
+  dfMound = []
+  dfWater = []
+  dfLeaf = []
+
+  width = width # dfGameHost Map Size
   y = ymin # Starting Y-Depth
   x = 0
 
   for line in map_data:
-    for char in line:
-      if char == "#":
-        dfWall.append((x,y))
-      elif char == ".":
+    for char in line[:width]:
+      if char == ".":
         pass
+
+      elif char == "#":
+        dfWall.append((x,y))
       elif char == ";":
         dfDark.append((x,y))
       elif char == "%":
         dfSpawn.append((x,y))
+
+      elif char == "*":
+        dfBush.append((x,y))
+      elif char == "=":
+        dfBranch.append((x,y))
+      elif char == ",":
+        dfLeaf.append((x,y))
+      elif char == ":":
+        dfMound.append((x,y))
+      elif char == "^":
+        dfWater.append((x,y))
+
       x += 1
     y += 1
     x = 0
 
-  return dfWall, dfDark, dfSpawn
-
-def dfMapPreview(dfWall, dfDark, dfSpawn):
-  view = ""
-  for y in range(30,168):
-    for x in range(40):
-      if (x,y) in dfWall:
-        view += "#"
-      elif (x,y) in dfDark:
-        view += ";"
-      elif (x,y) in dfSpawn:
-        view += "%"
-      else:
-        view += "."
-      if x == 39 and (y/5).is_integer():
-        view += " %s"%(y)
-    view += "\n"
-
-  print(view)
+  return dfWall, dfDark, dfSpawn, [dfBush, dfBranch, dfMound, dfWater, dfLeaf]
